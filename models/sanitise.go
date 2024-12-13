3db5890387e15a6e791aaf50dcfcee53d35ee393
Dee Kitchen <github@david.kitchen>
2024-12-13 11:03:04 +0000

    Revert removal of sanitize code as it broke other things... needs moar work

--- models/sanitise.go
+++ models/sanitise.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"unicode"
+
 	"github.com/microcosm-cc/bluemonday"
 )
 
@@ -15,21 +17,21 @@ var ASCIISpace = rune(` `[0])
 
 // CleanWord strips all HTML tags and whitespace
 func CleanWord(s string) string {
-	return textPolicy.Sanitize(s)
-	// TODO: return textPolicy.Sanitize(stripChars(s, true, false, false, true))
+	// return textPolicy.Sanitize(s)
+	return textPolicy.Sanitize(stripChars(s, true, false, false, true))
 }
 
 // CleanSentence strips all HTML tags, allows ASCII space
 func CleanSentence(s string, preventShouting bool) string {
-	return textPolicy.Sanitize(s)
-	// TODO: return textPolicy.Sanitize(stripChars(s, true, true, false, preventShouting))
+	// return textPolicy.Sanitize(s)
+	return textPolicy.Sanitize(stripChars(s, true, true, false, preventShouting))
 }
 
 // CleanBlockText allows everything, allows tabs, newlines, ASCII space but
 // non-conforming whitespace, control chars, and also prevents shouting
 func CleanBlockText(s string) string {
-	return textPolicy.Sanitize(s)
-	// TODO: return textPolicy.Sanitize(stripChars(s, true, true, true, true))
+	// return textPolicy.Sanitize(s)
+	return textPolicy.Sanitize(stripChars(s, true, true, true, true))
 }
 
 // SanitiseHTML sanitizes HTML
@@ -45,60 +47,60 @@ func SanitiseHTML(b []byte) []byte {
 	return htmlPolicy.SanitizeBytes(b)
 }
 
-// // stripChars will remove unicode characters according to the instructions given
-// // to it. This is strictly speaking a whitelist, it's less "strip", more "allow".
-// //
-// // With SanitiseText this should run before bluemonday (HTML sanitiser)
-// //
-// // With SanitiseHTML this should run before blackfriday (Markdown generator)
-// func stripChars(
-// 	in string,
-// 	allowPrint bool,
-// 	allowASCIISpace bool,
-// 	allowFormattingSpace bool,
-// 	preventShouting bool,
-// ) string {
-// 	tmp := []rune{}
+// stripChars will remove unicode characters according to the instructions given
+// to it. This is strictly speaking a whitelist, it's less "strip", more "allow".
+//
+// With SanitiseText this should run before bluemonday (HTML sanitiser)
+//
+// With SanitiseHTML this should run before blackfriday (Markdown generator)
+func stripChars(
+	in string,
+	allowPrint bool,
+	allowASCIISpace bool,
+	allowFormattingSpace bool,
+	preventShouting bool,
+) string {
+	tmp := []rune{}
 
-// 	isShouting := true
+	isShouting := true
 
-// 	for _, runeValue := range in {
-// 		var ret bool
-// 		// IsPrint covers anything actually printable plus ASCII space
-// 		if allowPrint && unicode.IsPrint(runeValue) {
-// 			if !allowASCIISpace && unicode.IsSpace(runeValue) {
-// 				continue
-// 			}
+	for _, runeValue := range in {
+		var ret bool
+		// IsPrint covers anything actually printable plus ASCII space
+		if allowPrint && unicode.IsPrint(runeValue) {
+			if !allowASCIISpace && unicode.IsSpace(runeValue) {
+				continue
+			}
 
-// 			if isShouting && preventShouting && unicode.IsLower(runeValue) {
-// 				isShouting = false
-// 			}
+			if isShouting && preventShouting && unicode.IsLower(runeValue) {
+				isShouting = false
+			}
 
-// 			ret = true
-// 		}
+			ret = true
+		}
 
-// 		// Only ASCII space
-// 		if allowASCIISpace && runeValue == ASCIISpace {
-// 			ret = true
-// 		}
+		// Only ASCII space
+		if allowASCIISpace && runeValue == ASCIISpace {
+			ret = true
+		}
 
-// 		// IsSpace covers tabs, newlines, ASCII space,etc
-// 		if allowFormattingSpace && unicode.IsSpace(runeValue) {
-// 			ret = true
-// 		}
+		// IsSpace covers tabs, newlines, ASCII space,etc
+		if allowFormattingSpace && unicode.IsSpace(runeValue) {
+			ret = true
+		}
 
-// 		if ret {
-// 			tmp = append(tmp, runeValue)
-// 		}
-// 	}
+		if ret {
+			tmp = append(tmp, runeValue)
+		}
+	}
 
-// 	if isShouting && preventShouting {
-// 		tmp2 := []rune{}
-// 		for _, runeValue := range string(tmp) {
-// 			tmp2 = append(tmp2, unicode.ToLower(runeValue))
-// 		}
-// 		tmp = tmp2
-// 	}
+	if isShouting && preventShouting {
+		tmp2 := []rune{}
+		for _, runeValue := range string(tmp) {
+			tmp2 = append(tmp2, unicode.ToLower(runeValue))
+		}
+		tmp = tmp2
+	}
 
-// 	return string(tmp)
-// }
+	return string(tmp)
+}
